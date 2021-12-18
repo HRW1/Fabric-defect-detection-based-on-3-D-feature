d7a2d15741648bde5605d906be7b01cc8a6777bc
Haoran Wen <50005328+HRW1@users.noreply.github.com>
2021-12-18 17:19:56 +0800

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/def_det.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/def_det.dir/main.cpp.obj"
+  "def_det.exe"
+  "def_det.exe.manifest"
+  "def_det.pdb"
+  "libdef_det.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/def_det.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/detect.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "E:/Clion/def_det/adaptive_threshold.cpp" "E:/Clion/def_det/cmake-build-debug/CMakeFiles/detect.dir/adaptive_threshold.cpp.obj"
+  "E:/Clion/def_det/clustering.cpp" "E:/Clion/def_det/cmake-build-debug/CMakeFiles/detect.dir/clustering.cpp.obj"
+  "E:/Clion/def_det/fuzzy_matching.cpp" "E:/Clion/def_det/cmake-build-debug/CMakeFiles/detect.dir/fuzzy_matching.cpp.obj"
+  "E:/Clion/def_det/main.cpp" "E:/Clion/def_det/cmake-build-debug/CMakeFiles/detect.dir/main.cpp.obj"
+  "E:/Clion/def_det/miscellaneous.cpp" "E:/Clion/def_det/cmake-build-debug/CMakeFiles/detect.dir/miscellaneous.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "D:/opencv/build1"
+  "D:/opencv/sources/include"
+  "D:/opencv/sources/modules/core/include"
+  "D:/opencv/sources/modules/flann/include"
+  "D:/opencv/sources/modules/imgproc/include"
+  "D:/opencv/sources/modules/ml/include"
+  "D:/opencv/sources/modules/photo/include"
+  "D:/opencv/sources/modules/dnn/include"
+  "D:/opencv/sources/modules/features2d/include"
+  "D:/opencv/sources/modules/imgcodecs/include"
+  "D:/opencv/sources/modules/videoio/include"
+  "D:/opencv/sources/modules/calib3d/include"
+  "D:/opencv/sources/modules/highgui/include"
+  "D:/opencv/sources/modules/objdetect/include"
+  "D:/opencv/sources/modules/stitching/include"
+  "D:/opencv/sources/modules/ts/include"
+  "D:/opencv/sources/modules/video/include"
+  "D:/opencv/sources/modules/gapi/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
